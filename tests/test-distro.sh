c55f637e13849ab5f83d67d74f979ceabe80c1c2
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-11-07 07:35:31 +0200

    For no reason....

--- tests/test-distro.sh
+++ tests/test-distro.sh
@@ -28,9 +28,9 @@ function create_machine() {
   tar cf - ./tests | lxc exec $NAME -- tar xvf - -C /tmp
   if [ "$#" -ne 1 ]
   then
-    lxc exec $NAME -- /bin/bash "touch /etc/environment"
-    lxc exec $NAME -- /bin/bash "echo HTTPS_PROXY=$2 >> /etc/environment"
-    lxc exec $NAME -- /bin/bash "echo https_proxy=$2 >> /etc/environment"
+    lxc exec $NAME -- /bin/bash -c "touch /etc/environment"
+    lxc exec $NAME -- /bin/bash -c "echo HTTPS_PROXY=$2 >> /etc/environment"
+    lxc exec $NAME -- /bin/bash -c "echo https_proxy=$2 >> /etc/environment"
   fi
   lxc exec $NAME -- /bin/bash "/tmp/tests/lxc/install-deps/$DISTRO"
 }
